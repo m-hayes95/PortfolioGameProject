59c91cb5fbc4bbafe7334d9bd5e3714de83e707d
Michael Hayes <115077851+m-hayes95@users.noreply.github.com>
2024-11-18 17:07:17 +0000

    Set up action queue for crane grabbing item

--- Assets/Scripts/Environment/CraneGame.cs
+++ Assets/Scripts/Environment/CraneGame.cs
@@ -1,4 +1,7 @@
+using System;
 using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using Vector3 = UnityEngine.Vector3;
 
@@ -14,8 +17,11 @@ namespace Environment
         private Rigidbody rb;
         [SerializeField] private bool useCraneControls;
         private bool upUsed, downUsed, rightUsed, activated;
+        [SerializeField]private bool moveTaskCompleted = false;
         private bool isCraneMoving = false;
         private Vector3 startPosition;
+        private Vector3 lowerPosition;
+        private Queue<Action> craneActions;
 
         private void Start()
         {
@@ -25,8 +31,49 @@ namespace Environment
             rightUsed = false;
             activated = false;
             startPosition = claw.transform.position;
+            craneActions = new Queue<Action>();
+            PopulateActionQueue();
+            StartCoroutine(HandleCraneNonPlayerActions());
         }
 
+        private void PopulateActionQueue()
+        {
+            // Used for all actions after the player had finished interacting with the claw machine
+            craneActions.Enqueue(LowerClaw);
+            craneActions.Enqueue(GrabWithClaw);
+            craneActions.Enqueue(RaiseClaw);
+            craneActions.Enqueue(ReturnClawHome);
+            craneActions.Enqueue(DropItem);
+        }
+        private IEnumerator HandleCraneNonPlayerActions()
+        {
+            float timeout = 10f;
+            float startTime = Time.time;
+            
+            for (int i = 0; craneActions.Count > 0; i++)
+            {
+                Action action = craneActions.Dequeue();
+                action.Invoke();
+                bool timedOut = false;
+                // Break if we get stuck in an action for too long
+                while (!moveTaskCompleted && !timedOut)
+                {
+                    if (Time.time - startTime > timeout)
+                    {
+                        timedOut = true;
+                    }
+                    yield return null;
+                }
+
+                if (timedOut)
+                {
+                    Debug.LogWarning("Timeout reached. Exiting the loop.");
+                    yield break;
+                }
+                // Reset time for each action
+                startTime = Time.time;
+            }
+        }
         private void Update()
         {
             if (!useCraneControls)
@@ -66,24 +113,48 @@ namespace Environment
         {
             rb.linearVelocity = direction * moveSpeed;
         }
+
+        private void LowerClaw()
+        {
+            moveTaskCompleted = false;
+            Debug.Log($"Lower claw");
+            // Set the position we will return to
+            startPosition = transform.position;
+            lowerPosition = new Vector3(0f, clawExtensionLength, 0f);
+            StartCoroutine(LerpClaw(transform.position, transform.position - lowerPosition, 0f, 2f));
+        }
         private void GrabWithClaw()
         {
-            // Create list of commands using queue for first in first out (Command Pattern?)
-            // Lower claw
-            // Grab
-            // Return Claw Home
-            // Drop Item
-            // Reset command queue
+            moveTaskCompleted = false;
+            Debug.Log($"Grab with claw");
+            StartCoroutine(Timer());
+        }
+
+        private void RaiseClaw()
+        {
+            moveTaskCompleted = false;
+            Debug.Log($"Raise claw");
+            StartCoroutine(LerpClaw(transform.position, startPosition, 0f, 2f));
         }
 
         private void ReturnClawHome()
         {
-            
+            moveTaskCompleted = false;
+            Debug.Log($"Return Home claw");
+            StartCoroutine(Timer());
         }
 
         private void DropItem()
         {
-            
+            moveTaskCompleted = false;
+            Debug.Log($"Drop item claw");
+        }
+
+        private IEnumerator Timer() // Used for testing -- Remove
+        {
+            yield return new WaitForSeconds(2f);
+            moveTaskCompleted = true;
+            Debug.Log($"Move task has been changed to true");
         }
         private IEnumerator LerpClaw(Vector3 startPos, Vector3 endPos, float delay, float lerpDuration)
         {
@@ -96,9 +167,9 @@ namespace Environment
                 );
                 yield return 0;
             }
+            moveTaskCompleted = true;
+            Debug.Log($"Move task has been changed to true");
             isCraneMoving = false;
         }
-        
-        
     }
 }
