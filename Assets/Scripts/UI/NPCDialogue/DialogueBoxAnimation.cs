e80e2a84e7b16d50c7e0ab02cabec7e046e1cf1c
Michael Hayes <115077851+m-hayes95@users.noreply.github.com>
2024-12-06 02:01:56 +0000

    Added animation with tweens

--- Assets/Scripts/UI/NPCDialogue/DialogueBoxAnimation.cs
+++ Assets/Scripts/UI/NPCDialogue/DialogueBoxAnimation.cs
@@ -6,15 +6,17 @@ namespace UI.NPCDialogue
     public class DialogueBoxAnimation : MonoBehaviour
     {
         [SerializeField] private GameObject dialogueBox;
+        [SerializeField] private float targetLocationY;
+        [SerializeField] private float tweenDuration;
 
         public void ShowDialogueBox()
         {
-            
+            dialogueBox.transform.DOMoveY(targetLocationY, tweenDuration).SetEase(Ease.OutBounce);
         }
 
         public void HideDialogueBox()
         {
-            
+            dialogueBox.transform.DOMoveY(-targetLocationY, tweenDuration);
         }
     }
 }
